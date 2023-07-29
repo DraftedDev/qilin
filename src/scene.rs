e2d0b65e66ace66e06696f7b11ea3cd0fbd0c36d
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2023-07-29 17:47:29 +0200

    Fixed inlines

--- src/scene.rs
+++ src/scene.rs
@@ -4,20 +4,15 @@ use crate::render::canvas::Canvas;
 
 /// Trait to represent a scene in the `Game`.
 pub trait Scene {
-    #[inline(never)]
     fn new() -> Self
     where
         Self: Sized;
 
-    #[inline(never)]
     fn enter(&mut self);
 
-    #[inline]
     fn update(&mut self, canvas: &mut Canvas, ctx: &mut GameContext);
 
-    #[inline]
     fn fixed_update(&mut self, canvas: &mut Canvas, ctx: &mut GameContext);
 
-    #[inline(never)]
     fn exit(&mut self);
 }
